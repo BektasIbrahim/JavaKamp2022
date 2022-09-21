f8338818b58d338603a74698ff988ce861c20c96
İbrahimB <bektasibrahim990@gmail.com>
2022-09-21 20:42:09 +0300

    1,Hafta

---
+++ javaHelloı/src/javaHelloı/Main.java
@@ -0,0 +1,10 @@
+package javaHelloı;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Merhaba World");
+
+	}
+
+}
---
+++ veriables/src/veriables/Main.java
@@ -0,0 +1,11 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 10;
+		String sms = "öğrenci sayısı : ";
+		System.out.println(sms + ogrenciSayisi);
+	}
+
+}
